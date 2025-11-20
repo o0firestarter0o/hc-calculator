6e844cd69fd6b2798e0ce9deccb1b930e1097cce
o0firestarter0o <sergiotur@gmail.com>
2025-11-20 14:59:29 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'efbs-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Archivos cacheados');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker activado');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
